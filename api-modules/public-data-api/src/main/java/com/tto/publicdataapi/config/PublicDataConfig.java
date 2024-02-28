7646ec10d8bdb4f328704b3890175fad90e28aef
PARK HYEONG JIN <70372188+legowww@users.noreply.github.com>
2024-02-28 17:59:05 +0900

    chore: 잘못 설정된 모듈 제거

--- api-modules/public-data-api/src/main/java/com/tto/publicdataapi/config/PublicDataConfig.java
+++
@@ -1,7 +0,0 @@
-package com.tto.publicdataapi.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-@EnableConfigurationProperties(PublicDataProperties.class)
-public class PublicDataConfig {
-}
